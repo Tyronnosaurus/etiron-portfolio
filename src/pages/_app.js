53527d16765b85cd9e287f662983a77bb3bfa140
Tyronnosaurus <Tyronnosaurus@protonmail.com>
2023-04-15 17:34:34 +0200

    Added colors and some style for App

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import '@/styles/globals.css'
-import {Montserrat} from "next/font/google"
+import { Montserrat } from "next/font/google"
+import Head from 'next/head'
 
 // Working with fonts in Next.js/Tailwind:  https://nextjs.org/docs/basic-features/font-optimization#with-tailwind-css
 const montserrat = Montserrat({
@@ -9,10 +10,17 @@ const montserrat = Montserrat({
 
 
 export default function App({ Component, pageProps }) {
-  return(
-    <main className={`${montserrat.variable} font-mont`}>
-      <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-    </main>
+      <main className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}>
+        <Component {...pageProps} />
+
+      </main>
+    </>
   )
 }
