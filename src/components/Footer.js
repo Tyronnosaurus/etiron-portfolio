49e15bf80a4934f1235715f8478d610016f9478b
Tyronnosaurus <Tyronnosaurus@protonmail.com>
2023-04-16 20:55:18 +0200

    Added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Layout from './Layout'
+import Link from 'next/link'
+
+
+const Footer = () => {
+  return (
+    <footer className='w-full border-t-2 border-solid border-dark font-medium text-lg'>
+        <Layout className='py-8 flex items-center justify-between'>
+            <span>&copy; {new Date().getFullYear()} All Rights Reserved.</span>
+            <div className='flex items-center'>
+                Built with <span className= 'text-2xl px-1'> &#9825; </span> by&nbsp;
+                <Link href='https://devdreaming.com' target='_blank'>CodeBucks</Link>, Eduard Tiron
+            </div>
+        </Layout>
+    </footer>
+  )
+}
+
+export default Footer
