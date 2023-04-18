c080c4264c7bf4d80687d494dc3c1907b4ab869b
Tyronnosaurus <Tyronnosaurus@protonmail.com>
2023-04-18 23:29:11 +0200

    Added Skills section

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,6 +5,7 @@ import Image from 'next/image'
 import React, { useEffect, useRef } from 'react'
 import ProfilePic from '../../public/images/profile/developer-pic-2.jpg'
 import { useMotionValue, useInView, useSpring } from 'framer-motion'
+import Skills from '@/components/Skills'
 
 
 
@@ -43,24 +44,24 @@ const about = () => {
         </Head>
         <main className='flex w-full flex-col items-center justify-center'>
             <Layout className='pt-16'>
-                <AnimatedText text="Passion Fuels Purpose!" className='mb-16'/>
+                <AnimatedText className='mb-16' text="Passion Fuels Purpose!" />
                 <div className='grid w-full grid-cols-8 gap-16'>
                     
                     <div className='col-span-3 flex flex-col items-start justify-start'>
                         <h2 className='mb-4 text-lg font-bold uppercase text-dark/75'>Biography</h2>
                         <p className='font-medium'>
-                           Hi, I'm Eduard Tiron, a web developer and UI/UX designer with a passion for creating beautiful, functional, 
-                           and user-centered digital experiences. With 4 years of experience in the field. I am always looking for 
-                           new and innovative ways to bring my clients' visions to life.
+                        Hi, I'm Eduard Tiron, a web developer and UI/UX designer with a passion for creating beautiful, functional, 
+                        and user-centered digital experiences. With 4 years of experience in the field. I am always looking for 
+                        new and innovative ways to bring my clients' visions to life.
                         </p>
                         <p className='font-medium my-4'>
-                           I believe that design is about more than just making things look pretty - it's about solving problems
-                           and creating intuitive, enjoyable experiences for users.
+                        I believe that design is about more than just making things look pretty - it's about solving problems
+                        and creating intuitive, enjoyable experiences for users.
                         </p>
                         <p className='font-medium'>
-                           Whether I'm working on a website, mobile app, or other digital products,
-                           I bring my commitment to design excellence and user-centered thinking to every project I work on.
-                           I look forward to the opportunity to bring my skills and passion to your next project.
+                        Whether I'm working on a website, mobile app, or other digital products,
+                        I bring my commitment to design excellence and user-centered thinking to every project I work on.
+                        I look forward to the opportunity to bring my skills and passion to your next project.
                         </p>
                     </div>
 
@@ -94,6 +95,9 @@ const about = () => {
                     </div>
 
                 </div>
+
+                <Skills />
+
             </Layout>
             
         </main>
