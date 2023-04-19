1581febdf1f6e7c02ddb642739225407b4dd30bd
Tyronnosaurus <Tyronnosaurus@protonmail.com>
2023-04-20 00:17:00 +0200

    Experience section: vertical bar, animations

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,11 +1,19 @@
 import React, { useRef } from 'react'
 import { motion, useScroll } from 'framer-motion'
+import LiIcon from './LiIcon'
 
 
 const Details = ({position, company, companyLink, time, address, work}) => {
+
+    const ref = useRef(null)
+
     return(
-        <li className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between'>
-            <div>
+        <li ref={ref} className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between'>
+            <LiIcon reference={ref}/>
+
+            <motion.div initial={{y:50}}
+                        whileInView={{y:0}}
+                        transition={{duration:0.5, type:"spring"}}>
                 <h3 className='capitalize font-bold text-2xl'>
                     {position} &nbsp; <a className='text-primary capitalize' href={companyLink} target='_blank'>@ {company}</a>
                 </h3>
@@ -15,7 +23,7 @@ const Details = ({position, company, companyLink, time, address, work}) => {
                 <p className='font-medium w-full'>
                     {work}
                 </p>
-            </div>
+            </motion.div>
         </li>
     )
 }
@@ -41,7 +49,7 @@ const Experience = () => {
 
                 {/* Vertical line */}
                 <motion.div style={{scaleY: scrollYProgress}}
-                            className='absolute left-8 top-0 w-[4px] h-full bg-dark origin-top'/>
+                            className='absolute left-9 top-0 w-[4px] h-full bg-dark origin-top'/>
 
                 {/* List of past experiences */}
                 <ul className='w-full flex flex-col items-start justify-between ml-4'>
