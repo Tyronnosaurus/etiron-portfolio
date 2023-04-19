3f604114e043fe993558d1a03b0c31fe3408df6d
Tyronnosaurus <Tyronnosaurus@protonmail.com>
2023-04-20 01:03:41 +0200

    Added Education section

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,6 +7,7 @@ import ProfilePic from '../../public/images/profile/developer-pic-2.jpg'
 import { useMotionValue, useInView, useSpring } from 'framer-motion'
 import Skills from '@/components/Skills'
 import Experience from '@/components/Experience'
+import Education from '@/components/Education'
 
 
 
@@ -99,6 +100,7 @@ const about = () => {
 
                 <Skills />
                 <Experience />
+                <Education />
 
             </Layout>
             
