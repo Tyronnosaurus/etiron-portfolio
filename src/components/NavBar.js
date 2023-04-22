cf2a0bf7dfa385443671f45b336659e87d609604
Tyronnosaurus <Tyronnosaurus@protonmail.com>
2023-04-22 18:03:57 +0200

    Corrected indentation

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -26,45 +26,47 @@ const CustomLink = ({href, title, className=""}) => {
 
 
 const NavBar = () => {
-  return (
-    <header className='w-full px-32 py-8 font-medium flex items-center justify-between'>
-        <nav>
-            <CustomLink href="/"         title="Home"     className='mr-4'/>
-            <CustomLink href="/about"    title="About"    className='mx-4'/>
-            <CustomLink href="/projects" title="Projects" className='mx-4'/>
-            <CustomLink href="/articles" title="Articles" className='ml-4'/>
-        </nav>
+
+    return (
+        <header className='w-full px-32 py-8 font-medium flex items-center justify-between'>
+            <nav>
+                <CustomLink href="/"         title="Home"     className='mr-4'/>
+                <CustomLink href="/about"    title="About"    className='mx-4'/>
+                <CustomLink href="/projects" title="Projects" className='mx-4'/>
+                <CustomLink href="/articles" title="Articles" className='ml-4'/>
+            </nav>
 
 
-        <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
-            <Logo/>
-        </div>
+            <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
+                <Logo/>
+            </div>
 
-        <nav className='flex items-center justify-center flex-wrap'>
-            <motion.a href="https://twitter.com"
-                      target={"_blank"}
-                      className='w-6 mr-3'
-                      whileHover={{y:-2}}
-                      whileTap={{scale:0.9}}>
-                <TwitterIcon />
-            </motion.a>
-            <motion.a href="https://linkedin.com/in/eduardtiron/?locale=en_US"
-                      target={"_blank"}
-                      className='w-6 mx-3'
-                      whileHover={{y:-2}}
-                      whileTap={{scale:0.9}}>
-                <LinkedInIcon />
-            </motion.a>
-            <motion.a href="https://github.com/Tyronnosaurus"
-                      target={"_blank"}
-                      className='w-6 ml-3'
-                      whileHover={{y:-2}}
-                      whileTap={{scale:0.9}}>
-                <GithubIcon />
-            </motion.a>
-        </nav>
-    </header>
-  )
+            <nav className='flex items-center justify-center flex-wrap'>
+                <motion.a href="https://twitter.com"
+                        target={"_blank"}
+                        className='w-6 mr-3'
+                        whileHover={{y:-2}}
+                        whileTap={{scale:0.9}}>
+                    <TwitterIcon />
+                </motion.a>
+                <motion.a href="https://linkedin.com/in/eduardtiron/?locale=en_US"
+                        target={"_blank"}
+                        className='w-6 mx-3'
+                        whileHover={{y:-2}}
+                        whileTap={{scale:0.9}}>
+                    <LinkedInIcon />
+                </motion.a>
+                <motion.a href="https://github.com/Tyronnosaurus"
+                        target={"_blank"}
+                        className='w-6 ml-3'
+                        whileHover={{y:-2}}
+                        whileTap={{scale:0.9}}>
+                    <GithubIcon />
+                </motion.a>
+
+            </nav>
+        </header>
+    )
 }
 
 export default NavBar
