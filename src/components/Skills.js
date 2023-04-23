e40589a8efe975c90985d870d46fbf232a37a676
Tyronnosaurus <Tyronnosaurus@protonmail.com>
2023-04-23 23:10:21 +0200

    Simplified skills section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -8,7 +8,7 @@ const Skill = ({name, x, y}) => {
                 whileHover={{scale:1.05}}
                 initial={{x:0, y:0}}
                 whileInView={{x:x, y:y}}
-                transition={{duration:1.5}}
+                // transition={{duration:0.5}}
                 viewport={{once:true}}>
         {name}
     </motion.div>
@@ -25,11 +25,7 @@ const Skills = () => {
         {/* bg-circularLight defined in tailwind.config.js (created with https://developer.mozilla.org/en-US/docs/Web/CSS/gradient/repeating-radial-gradient) */}
         <div className='w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight'>
 
-            <motion.div className='flex items-center justify-center rounded-full font-semibold bg-dark text-light p-8 shadow-dark cursor-pointer'
-                        whileHover={{scale:1.05}}>
-                Web
-            </motion.div>
-
+            <Skill name="Web"        x="0vw"   y="0vw" />
             <Skill name="HTML"       x="-25vw" y="2vw" />
             <Skill name="CSS"        x="-5vw"  y="-10vw" />
             <Skill name="Javascript" x="20vw"  y="6vw" />
