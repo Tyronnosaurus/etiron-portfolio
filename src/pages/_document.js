61a26a22e3697dde3c4ee7d1ba4a1b70830d1cef
Tyronnosaurus <Tyronnosaurus@protonmail.com>
2023-04-23 22:27:55 +0200

    Prevent flash of unstyled content on page load

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,10 +1,23 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        
+        {/* Prevents flash of unstyled content (FOUC) on page load, i.e. style is set to dark but it takes a second to apply the theme */}
+        <Script id="theme-switcher">
+          {`
+            if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+              document.documentElement.classList.add('dark')
+            } else {
+              document.documentElement.classList.remove('dark')
+            }
+          `}
+        </Script>
+        
         <Main />
         <NextScript />
       </body>
