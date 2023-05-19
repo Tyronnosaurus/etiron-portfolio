a4c615cfd7f04bd68b3c0ddacb336fe8f19b7584
Tyronnosaurus <Tyronnosaurus@protonmail.com>
2023-05-19 23:52:46 +0200

    Added screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,9 @@ module.exports = {
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+
   darkMode: 'class',
+
   theme: {
     extend: {
       fontFamily: {
@@ -23,8 +25,17 @@ module.exports = {
         circularLight: 'repeating-radial-gradient(rgba(0,0,0,0.4)       2px, #f5f5f5 5px, #f5f5f5 100px);',
         circularDark:  'repeating-radial-gradient(rgba(255,255,255,0.5) 2px, #1b1b1b 8px, #1b1b1b 100px);'
       }
+    },
+    screens: {
+      'xs': '480px',   // => @media (min-width: 480px) { ... }
+      'sm': '640px',   // => @media (min-width: 640px) { ... }
+      'md': '768px',   // => @media (min-width: 768px) { ... }
+      'lg': '1024px',  // => @media (min-width: 1024px) { ... }
+      'xl': '1280px',  // => @media (min-width: 1280px) { ... }
+      '2xl': '1536px', // => @media (min-width: 1536px) { ... }
     }
   },
+
   
   plugins: []
 }
