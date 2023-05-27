25d89c9f87014746a4a561214248949cfcf834c2
Tyronnosaurus <Tyronnosaurus@protonmail.com>
2023-05-27 12:34:38 +0200

    Solve no-unescaped-entities compile error

--- src/pages/about.js
+++ src/pages/about.js
@@ -53,16 +53,16 @@ const about = () => {
                     <div className='col-span-3 flex flex-col items-start justify-start'>
                         <h2 className='mb-4 text-lg font-bold uppercase'>Biography</h2>
                         <p className='font-medium'>
-                        Hi, I'm Eduard Tiron, a web developer and UI/UX designer with a passion for creating beautiful, functional, 
-                        and user-centered digital experiences. With 4 years of experience in the field. I am always looking for 
-                        new and innovative ways to bring my clients' visions to life.
+                        Hi, I&apos;m Eduard Tiron, a web developer and UI/UX designer with a passion for creating beautiful, functional, 
+                        and user-centered digital experiences. With 4 years of experience in the field. I&apos;m always looking for 
+                        new and innovative ways to bring my clients&apos; visions to life.
                         </p>
                         <p className='font-medium my-4'>
-                        I believe that design is about more than just making things look pretty - it's about solving problems
+                        I believe that design is about more than just making things look pretty - it&apos;s about solving problems
                         and creating intuitive, enjoyable experiences for users.
                         </p>
                         <p className='font-medium'>
-                        Whether I'm working on a website, mobile app, or other digital products,
+                        Whether I&apos;m working on a website, mobile app, or other digital products,
                         I bring my commitment to design excellence and user-centered thinking to every project I work on.
                         I look forward to the opportunity to bring my skills and passion to your next project.
                         </p>
