8a611fb39d2205f146af08b2d5701a99b95b1bc5
Tyronnosaurus <Tyronnosaurus@protonmail.com>
2023-05-27 21:47:09 +0200

    Made footer responsive, deleted 'Made by X'

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,14 +5,18 @@ import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <footer className='w-full border-t-2 border-solid border-dark font-medium text-lg dark:text-light dark:border-light'>
-        <Layout className='py-8 flex items-center justify-between'>
+    <footer className='w-full font-medium  dark:text-light
+                       text-base md:text-lg
+                       border-t-2 border-solid border-dark dark:border-light'>
+        
+        <Layout className='items-center justify-centerx md:justify-between
+                           flex flex-col md:flex-row
+                           md:p-8 lg:p-8 xl:p-8'>
+
             <span>&copy; {new Date().getFullYear()} All Rights Reserved.</span>
-            <div className='flex items-center'>
-                Built with <span className= 'text-2xl px-1'> &#9825; </span> by&nbsp;
-                <Link href='https://devdreaming.com' target='_blank'>CodeBucks</Link>, Eduard Tiron
-            </div>
+
         </Layout>
+
     </footer>
   )
 }
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,8 +3,8 @@ import React from 'react'
 
 const Layout = ({children, className,}) => {
   return (
-    <div className={`w-full h-full inline-block z-0 bg-light dark:bg-dark ${className}
-                     p-8 md:p-12 lg:p-16 xl:p-24}`}>
+    <div className={`w-full h-full inline-block z-0 bg-light dark:bg-dark
+                     p-8 md:p-12 lg:p-16 xl:p-24 ${className}`}>
         {children}
     </div>
   )
